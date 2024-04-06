0fffe7a5221e4b08749f9a21d2f9995d1f63764c
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-04-06 11:25:47 +0100

    Add my_each_with_index and my_select methods to Enumerable

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,25 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    i = 0
+    while i < self.size
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    result = []
+    i = 0
+    while i < self.size
+      if yield(self[i])
+        result << self[i]
+      end
+      i += 1
+    end
+    result
+  end
 end
 
 # You will first have to define my_each
@@ -10,7 +30,9 @@ class Array
   def my_each
     i = 0
     while i < self.size
-
+      yield(self[i])
+      i += 1
     end
+    self
   end
 end
