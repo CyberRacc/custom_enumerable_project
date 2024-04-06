0e6e52f908e6bf0ac5dffd6b3cbf02d82122dc23
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-04-06 15:00:26 +0100

    Add custom implementation of Enumerable module

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,8 +1,13 @@
+# frozen_string_literal: true
+
+# My own implementation of the Enumerable module
+
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Lint/MissingCopEnableDirective, Metrics/MethodLength
 module Enumerable
   # Your code goes here
   def my_each_with_index
     i = 0
-    while i < self.size
+    while i < size
       yield(self[i], i)
       i += 1
     end
@@ -12,13 +17,70 @@ module Enumerable
   def my_select
     result = []
     i = 0
-    while i < self.size
-      if yield(self[i])
-        result << self[i]
-      end
+    while i < size
+      result << self[i] if yield(self[i])
       i += 1
     end
     result
+  end
+
+  def my_all?
+    return true if self == []
+
+    i = 0
+    if block_given?
+      while i < size
+        return false unless yield(self[i]) == true
+
+        i += 1
+      end
+    else
+      while i < size
+        return false if self[i] == false || self[i].nil?
+
+        i += 1
+      end
+    end
+    true
+  end
+
+  def my_any?
+    return false if self == []
+
+    i = 0
+    if block_given?
+      while i < size
+        return true unless yield(self[i]) == false || yield(self[i]).nil?
+
+        i += 1
+      end
+    else
+      while i < size
+        return true if self[i]
+
+        i += 1
+      end
+    end
+    false
+  end
+
+  def my_none?
+    i = 0
+    if block_given?
+      while i < size
+        # returns true unless the block
+        return false if yield(self[i])
+
+        i += 1
+      end
+    else
+      while i < size
+        return false if self[i]
+
+        i += 1
+      end
+    end
+    true
   end
 end
 
@@ -29,7 +91,7 @@ end
 class Array
   def my_each
     i = 0
-    while i < self.size
+    while i < size
       yield(self[i])
       i += 1
     end
